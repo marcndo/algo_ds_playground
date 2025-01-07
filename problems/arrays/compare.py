4a0fdd120d2c1b7ad132cce2040b5fa6c4e144ae
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-01-07 18:25:40 +0100

    implement compare function using python build ins

--- problems/arrays/compare.py
+++ problems/arrays/compare.py
@@ -1,3 +1,4 @@
+import time 
 """Write a scans two arrays and return true if there's a match or false
 when there's no match
 """
@@ -15,8 +16,8 @@ with space complexity of o(1)"""
 #                 return True
 #     return False
 
-arr1 = ['jane', 'paul', 'jude', 'Leonard']
-arr2 = ['Peter', '', 'james']
+arr1 = [2, 5, 8, 3, 9]
+arr2 = [1, 2, 7, 11, 12, 18] * 10000000
 
 
 #Solution two
@@ -26,14 +27,32 @@ Looking at the function below, the time complexity is o(len(arr1) + len(arr2))
 while the space complexity is o(len(arr1)).
 This code does well in optimizing the time complexity but poorly in space complexity.
 """
+# def compare_arrays(arr1, arr2):
+#     map_elements = {}
+#     for i in arr1:
+#         map_elements[i] = True
+#     for j in arr2:
+#         if j in map_elements.keys():
+#             return True
+#     return False
+
+#Solution three
+"""Another way is to convert one of the array to set.
+The time complexity of the code below is O(len(arr1_set) + len(arr2))
+
+"""
+# def compare_arrays(arr1, arr2):
+#     arr1_set = set(arr1)
+#     for j in arr2:
+#         if j in arr1_set:
+#             return True
+#     return False 
+
+
 def compare_arrays(arr1, arr2):
-    map_elements = {}
-    for i in arr1:
-        map_elements[i] = True
-    for j in arr2:
-        if j in map_elements.keys():
-            return True
-    return False
+    return list(set(arr1).intersection(set(arr2)))
 
 
+start_time = time.time()
 print(compare_arrays(arr1, arr2))
+
