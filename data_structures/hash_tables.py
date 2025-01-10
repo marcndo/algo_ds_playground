1292b01f6977a4b110030dd682a50d38f332ffce
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-01-10 18:14:43 +0100

    Implement a hash table

---
+++ data_structures/hash_tables.py
@@ -0,0 +1,32 @@
+class HashTables:
+    def __init__(self, size):
+        self.data = [None] * size
+
+#the hash function
+    def _hash(self, key):
+        hash_value = 0
+        for i in range(len(key)):
+            hash_value = (hash_value + ord(key[i]) * i) % len(self.data)
+        return hash_value
+    
+# set values to keys
+    def set(self, key, value):
+        address = self._hash(key)
+        if not self.data[address]:
+            self.data[address] = []
+        self.data[address].append([key, value])
+
+# get values
+def get(self, key):
+    address = self._hash(key)
+    current_bucket = self.data[address]
+    if current_bucket:
+        for i in range(len(self.data)):
+            if current_bucket[i][0] == key:
+                return current_bucket[i][1]
+    return None
+
+hash_table = HashTables(50)
+hash_table.set('name', 'Jane')
+hash_table.set('score', 20)
+print(hash_table.get('score'))
