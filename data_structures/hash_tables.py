d5aedee0a87955c659e8e6babb0203c8ce4b20b8
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-01-11 17:44:54 +0100

    feat: Add keys and values retrival methods

--- data_structures/hash_tables.py
+++ data_structures/hash_tables.py
@@ -25,9 +25,31 @@ class HashTables:
                 if current_bucket[i][0] == key:
                     return current_bucket[i][1]
         return None
+    
+    #get keys
+    def get_keys(self):
+        keys = []
+        for i in range(len(self.data)):
+            if self.data[i]:
+                keys.append(self.data[i][0][0])
+        return keys
+    
+    #Get values
+    def get_values(self):
+        values = []
+        for i in range(len(self.data)):
+            if self.data[i]:
+                values.append(self.data[i][0][1])
+        return values
+    
 
 hash_table = HashTables(50)
 hash_table.set('name', 'Jane')
 hash_table.set('score', 20)
+hash_table.set('age', 28)
+hash_table.set('department', 'Mathematics')
+hash_table.set('course', 'MAT301')
 print(hash_table.get('score'))
 print(hash_table.get('name'))
+print(hash_table.get_keys())
+print(hash_table.get_values())
