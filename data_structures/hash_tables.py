99e6288f02bdd31c60bccd4f3fae414ff5e2244b
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-01-11 16:27:47 +0100

    fixup: Update hashing function and reformate code

--- data_structures/hash_tables.py
+++ data_structures/hash_tables.py
@@ -6,7 +6,7 @@ class HashTables:
     def _hash(self, key):
         hash_value = 0
         for i in range(len(key)):
-            hash_value = (hash_value + ord(key[i]) * i) % len(self.data)
+            hash_value = (hash_value + ord(key[i]) * (i+1)) % len(self.data)
         return hash_value
     
 # set values to keys
@@ -17,16 +17,17 @@ class HashTables:
         self.data[address].append([key, value])
 
 # get values
-def get(self, key):
-    address = self._hash(key)
-    current_bucket = self.data[address]
-    if current_bucket:
-        for i in range(len(self.data)):
-            if current_bucket[i][0] == key:
-                return current_bucket[i][1]
-    return None
+    def get(self, key):
+        address = self._hash(key)
+        current_bucket = self.data[address]
+        if current_bucket:
+            for i in range(len(self.data)):
+                if current_bucket[i][0] == key:
+                    return current_bucket[i][1]
+        return None
 
 hash_table = HashTables(50)
 hash_table.set('name', 'Jane')
 hash_table.set('score', 20)
 print(hash_table.get('score'))
+print(hash_table.get('name'))
