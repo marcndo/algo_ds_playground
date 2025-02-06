4de1a979aa9f0c6756f3cb44977d70e28f7deb68
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-02-06 17:55:41 +0100

    Implement second approach to bubble sort

--- algorithms/sorting/bubblesort.py
+++ algorithms/sorting/bubblesort.py
@@ -1,5 +1,7 @@
+import random
 # Simple bubble sort without optimization
-array = [5, 2, 6, 1, 9, 0, 1]
+array1 = [5, 2, 6, 1, 9, 0, 1]
+array = random.sample(range(10), 10)
 def bubble_sort(array):
     length = len(array)
     for i in range(length):
@@ -9,3 +11,24 @@ def bubble_sort(array):
                 array[j] = array[j+1]
                 array[j+1] = temp
     return array
+
+# Second approach
+def optimize_bubble_sort(array):
+    length = len(array)
+    for i in range(length):
+        swaped = False
+        for j in range(length - i - 1):
+            if array[j] > array[j+1]:
+                temporal = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temporal
+                swaped = True
+        if not swaped:
+            break
+    return array
+
+
+print(array)
+print(optimize_bubble_sort(array))
+print(bubble_sort(array))
+
