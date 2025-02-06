350b8a8a05a8b3eb533b35b2e694e2bbfc52afe8
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-02-06 19:41:22 +0100

    Implement selection sort

---
+++ algorithms/sorting/selection_sort.py
@@ -0,0 +1,21 @@
+import random
+def selection_sort(array):
+    length = len(array)
+    for i in range(length):
+        min_index = i
+        temp = array[i]
+        swaped = False
+        for j in range(i+1,length):
+            if array[j] < array[min_index]:
+                min_index = j
+        array[i] = array[min_index]
+        array[min_index] = temp
+        swaped = True
+        if not swaped:
+            break
+    return array
+                
+
+array = random.sample(range(10), 8)
+print(array)
+print(selection_sort(array))
