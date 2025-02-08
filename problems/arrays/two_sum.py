be5e0aae9fe9d824de3603c9bd84ae7e89f14adc
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-02-08 17:09:05 +0100

    Refactor opt_find_indice function and remove uncessary files

--- problems/arrays/two_sum.py
+++ problems/arrays/two_sum.py
@@ -7,17 +7,18 @@ def find_two_sum(array, a):
 
 
 # We use hash map to solve this problem.
-def opt_find_indice(array, a):
+def opt_find_indice(array, target):
     hash_map = {}
     for i in range(len(array)):
-        if not array[i] in hash_map:
-            hash_map[a-array[i]] = i
+        current_key = array[i]
+        if not current_key in hash_map:
+            hash_map[target - current_key] = i
         else:
-            return [hash_map[array[i]], i]
+            return [hash_map[current_key], i]
     
 
     
 
-print(find_two_sum([1, 3, 5, 9, 2], 11))
+print(find_two_sum([2,7,11,15], 9))
 
-print(opt_find_indice([1, 3, 5, 9, 2], 11))
+print(opt_find_indice([2,7,11,15], 9))
