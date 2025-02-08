e459ac6c62ab33a576f91dfe1137aa23fa5b3438
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-02-08 04:59:52 +0100

    optimize function that find indices that adds to a sum with hash map

--- problems/arrays/two_sum.py
+++ problems/arrays/two_sum.py
@@ -4,7 +4,20 @@ def find_two_sum(array, a):
         for j in range(i+1, len(array)):
             if array[j] == a - array[i]:
                 return [i, j]
+
+
+# We use hash map to solve this problem.
+def opt_find_indice(array, a):
+    hash_map = {}
+    for i in range(len(array)):
+        if not array[i] in hash_map:
+            hash_map[a-array[i]] = i
+        else:
+            return [hash_map[array[i]], i]
     
 
+    
 
-print(find_two_sum([], 89))
+print(find_two_sum([1, 3, 5, 9, 2], 11))
+
+print(opt_find_indice([1, 3, 5, 9, 2], 11))
