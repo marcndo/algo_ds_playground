6ec8075857dcdf497f7a2e6143b811e806bc24d5
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-02-10 22:41:46 +0100

    Optimize container with largest water content  code using shifting pointers

--- problems/arrays/largest_container.py
+++ problems/arrays/largest_container.py
@@ -1,13 +1,33 @@
 # Question: https://leetcode.com/problems/container-with-most-water/description/
-a = [1,8,6,2,5,4,8,3,7]
+a = [1,8,6, 4, 2, 0, 8, 1, 10]
 
+#Brute force approach [1,8,6]
 def container_with_max_water(array):
     maximum_area = 0
     for i in range(len(array)):
         for j in range(i+1, len(array)):
             area = min(array[i], array[j]) * (j - i)
-            if area > maximum_area: 
+            if area > maximum_area:
                 maximum_area = area
     return f'{maximum_area}m^2'
-    
+
+# Shifting pointers approach a = [1,8,6]
+def opt_container_with_max_water(array):
+    pointer_1 = 0 
+    pointer_2 = len(array)-1 
+    maximum_area = 0 
+    area = min(array[pointer_1], array[pointer_2]) * (pointer_2 - pointer_1) #1
+    maximum_area = area
+    while pointer_1 < pointer_2:
+        area = min(array[pointer_2], array[pointer_1]) * (pointer_2 - pointer_1)
+        if area > maximum_area:
+            maximum_area = area
+        if array[pointer_1] > array[pointer_2]:
+            pointer_2 -=1
+        else:
+            pointer_1 +=1
+        
+    return f'{maximum_area}m^2'
+
+print(opt_container_with_max_water(a))
 print(container_with_max_water(a))
