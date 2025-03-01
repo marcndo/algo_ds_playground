48f0246873a7a8f1d34d222f17765c81c2a11bd5
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-03-01 22:53:41 +0100

    Implement stack in an alternative way

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,9 +1,10 @@
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, data):
+        self.data = data
         self.next = None
+        
     def __str__(self):
-        return str(self.value)
+        return str(self.data)
 
 class Stacks:
     def __init__(self):
@@ -47,10 +48,53 @@ class Stacks:
             self.top = self.top.next
         self.length -=1
 
+# A simplier version of stack
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            holding_pointer = self.head
+            self.head = new_node
+            new_node.next = holding_pointer
+
+    def pop(self):
+        if not self.head:
+            return None
+        else:
+            holding_pointer = self.head
+            self.head = self.head.next
+
+    def peek(self):
+        return self.head.data
+
+    def __str__(self):
+        nodes = []
+        current = self.head
+        while current:
+            nodes.append(str(current.data))
+            current = current.next
+        return "->".join(nodes)
+
 stack = Stacks()
 stack.push_item(4)
 stack.push_item(9)
 stack.push_item(8)
+print(stack.peek())
+ # stack.pop()
+print(stack)
+stack = Stack()
+stack.push('Jude')
+stack.push('Merveille')
+stack.push('Jean')
+stack.push('Laura')
+stack.push('Jeremy')
 stack.pop()
+stack.pop()
+print(stack.peek())
 print(stack)
 
