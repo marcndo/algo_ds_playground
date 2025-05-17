498470ccf47b80d85806701f962ed3eec539a3b7
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-05-17 03:05:41 +0100

    Solve merge two sorted array problem

--- problems/arrays/merge_arrays.py
+++ problems/arrays/merge_arrays.py
@@ -1,9 +1,35 @@
-#Write a function to merge two sorted arrays and remain sorted
-"""one way is to create an empty list, loop throw the elements, campare 
- them and append them to created list
+"""Write a function to merge two sorted arrays and remain sorted
+https://leetcode.com/problems/merge-sorted-array/description/
 """
-def merge_arrays(array1, array2):
-    return sorted(array1 + array2)
 
 
-print(merge_arrays([1, 7, 9, 4], [2, 8, 5]))
+#Brute force approach
+#Time complexity (m+n)log(m+n)
+def merge_arrays(array1, m, array2, n):
+   if not isinstance(array1, list) or isinstance(array2, list):
+       print("Input incorrect")
+   array1 = array1[:m] + array2
+   array1.sort()
+   for i in range(m + n):
+       array1[i] = array1[i]
+
+
+def optimal_solution(array1, m, array2, n):
+    i, j, k = m-1, n-1, m+n-1
+    while i >= 0 and j >= 0:
+        if array1[i] > array2[j]:
+            array1[k] = array1[i]
+            i -= 1
+        else:
+            array1[k] = array2[j]
+            j -= 1
+        k -= 1
+
+    while j >= 0:
+        array1[k] = array2[j]
+        k -= 1
+        j -= 1
+
+
+print(merge_arrays([1, 4, 7, 8, 0, 0, 0], 4, [2, 4, 5], 3))
+print(optimal_solution([1, 4, 7, 8, 0, 0, 0], 4, [2, 4, 5], 3))
