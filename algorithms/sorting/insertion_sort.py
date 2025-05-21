fd94afde784dd155c323c3d7541bd7667b46289e
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-05-21 04:57:47 +0100

    Implement insertion sort

--- algorithms/sorting/insertion_sort.py
+++ algorithms/sorting/insertion_sort.py
@@ -1,17 +1,35 @@
 import random
 # Best in data set that is almost sorted
 
-def insertion_sort(array):
-    for i in range(1, len(array)):
+"""
+function insertion_sort(arr)
+    length = len(arr)
+    if length <= 1
+    return arr
+    for i in range(1, length)
+        current_value = arr[i]
         j = i - 1
-        key = array[i]
-        while j >= 0 and array[j] > key:
-            array[j+1] = array[j]
-            j -=1
-        array[j+1] = key
-    return array
+        while j >= 0 and arr[j] > current_value:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j + 1] = current_value
+"""
+
+
+def insertion_sort(arr):
+    length = len(arr)
+    for i in range(1, length):
+        j = i - 1
+        current_value = arr[i]
+        while j >= 0 and arr[j] > current_value:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = current_value
+    return arr
 
 
 array = random.sample(range(10), 10)
+print("===Before sorting===")
 print(array)
+print("====After sorting===")
 print(insertion_sort(array))
