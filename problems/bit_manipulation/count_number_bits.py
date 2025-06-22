5e4e0061bb99c4eae8ac831a1c98b9ddfc0e77bd
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-06-22 03:48:32 +0100

    Count number of ones

---
+++ problems/bit_manipulation/count_number_bits.py
@@ -0,0 +1,52 @@
+"""
+https://leetcode.com/problems/number-of-1-bits/description/
+Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+
+
+Example 1:
+
+Input: n = 11
+
+Output: 3
+
+Explanation:
+
+The input binary string 1011 has a total of three set bits.
+
+Example 2:
+
+Input: n = 128
+
+Output: 1
+
+Explanation:
+
+The input binary string 10000000 has a total of one set bit.
+
+Example 3:
+
+Input: n = 2147483645
+
+Output: 30
+
+Explanation:
+
+The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+
+
+
+Constraints:
+
+1 <= n <= 231 - 1
+
+"""
+
+def count_ones(n):
+    result = 0
+    while n:
+        result += n % 2
+        n = n >> 1
+    return result
+
+print(count_ones(2147483645))
