ba967be5a33c7e8b196a8c131a7448e2a205ba03
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-09-17 19:04:38 +0100

    Guess a number

---
+++ problems/search/number_guess.py
@@ -0,0 +1,17 @@
+def guess(n):
+    pass
+
+def guess_number(n):
+    left, right = 1, n
+    while left <= right:
+        mid = (left + right) // 2
+        result = guess(mid)
+        if result == 0:
+            return mid
+        elif result == -1:
+            left = mid + 1
+        else:
+            right = mid - 1
+        
+
+
