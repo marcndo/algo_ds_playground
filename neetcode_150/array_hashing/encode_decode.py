e029c6296edd3b98734f2b324a969f0a76babf4e
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-09-17 12:30:01 +0100

    Alternative decoder

--- neetcode_150/array_hashing/encode_decode.py
+++ neetcode_150/array_hashing/encode_decode.py
@@ -14,6 +14,23 @@ def decode(strs):
         decoded.append(strs[i:i+length])
         i += length
     return decoded
+
+
+def decoder(strs):
+    decoded_list = []
+    i = 0
+    while i < len(strs):
+        j = i
+        while strs[j] != "#":
+            j += 1
+        length = int(strs[i:j])
+        i = j + 1
+        decoded_list.append(strs[i:i+length])
+        i += length
+    return decoded_list
+        
+
+    
     
 
 
@@ -21,4 +38,5 @@ strs = ["I", "love", "neetcode"]
 cod = "1#I4#love8#neetcode"
 print(encoder(strs))
 print(decode(cod))
+print(decoder(cod))
 
