66fe5ee86b1c051ccb0396b00b4c89ba7d37360b
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-09-20 20:44:37 +0100

    Alternative implementation of top k freq element

--- neetcode_150/array_hashing/top_k_freq_elem.py
+++ neetcode_150/array_hashing/top_k_freq_elem.py
@@ -1,3 +1,4 @@
+import heapq
 nums = [2, 1, 3, 2, 3, 2, 2, 3, 1, 0, 0, 0, 0, 0, 5]
 k = 2
 result = [0, 2]
@@ -27,9 +28,21 @@ def topFreqElement(nums, k):
             if len(result) == k:
                 return result
 
-    #we use counting sort to pick top k element
-    
+def topFreqElm(nums, k):
+    counter = {}
+    for num in nums:
+        counter[num] = 1 + counter.get(num, 0)
+    min_heap = []
+    for num, freq in counter.items():
+        if len(min_heap) < k:
+            heapq.heappush(min_heap, (freq, num))
+        elif min_heap[0][0] < freq:
+            heapq.heappop(min_heap)
+            heapq.heappush(min_heap, (freq, num))
+    return [num for freq, num in min_heap]
+           
 
 
 print(top_freq_elem(nums, 2))
 print(topFreqElement(nums, 2))
+print(topFreqElm(nums, 2))
