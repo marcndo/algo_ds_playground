f182df42d06e6ffd2c5019ca1325cdefcf02a268
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-10-01 10:35:10 +0100

    Correct two pointer approach in max_area.py

--- neetcode_150/two_pointers/max_area.py
+++ neetcode_150/two_pointers/max_area.py
@@ -1,16 +1,27 @@
-# We can maximize the area by maximizing the width and the height
-# Since the height would always keep getting smaller as the pointer move
-# We focus on maximizing the heights
-def max_area(nums):
-    max_area = 0
-    left, right = 0, len(nums) - 1
-    while left < right:
-        current_area = min(nums[left], nums[right]) * (right - left)
-        max_area = max(max_area, current_area)
-        if nums[left] < nums[right]:
-            left += 1
-        else:
-            right -= 1
-    return max_area
 
-print(max_area([1,8,6,2,5,4,8,3,7]))
+def max_area(nums):
+    pass # prefix and suffix maxes approach with O(n) space complexity
+
+
+
+#Water quantity would always depend on the min(max_left, max_right)
+def maxArea(heights):
+    l, r = 0, len(heights) - 1
+    result = 0
+    max_left, max_right = heights[l], heights[r]
+    while l < r:
+        if max_left < max_right:
+            l += 1
+            max_left = max(max_left, heights[l])
+            result += max_left - heights[l]
+        else:
+            r -= 1
+            max_right = max(max_right, heights[r])
+            result += max_right - heights[r]
+    return result 
+
+
+
+print(max_area([0,2,0,3,1,0,1,3,2,1]))
+
+print(maxArea([0,2,0,3,1,0,1,3,2,1]))
