d3223e49534e4649d0f7a1a9e5e3d5064680c46d
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-10-09 03:03:41 +0100

    Find longest non repeated substring

--- neetcode_150/sliding_window/longest_substring.py
+++ neetcode_150/sliding_window/longest_substring.py
@@ -1,34 +1,32 @@
-def lengthLongestSubstring(s):
+def length_of_longest_substring(s):
     max_length = 0
-    for i in range(len(s)):
+    length = len(s)
+    for i in range(length):
         char_set = set()
-        for j in range(i, len(s)):
-            current_char = s[j]
-            if current_char in char_set:
+        current_count = 0
+        for j in range(i, length):
+            if s[j]  in char_set:
                 break
             else:
-                char_set.add(current_char)
-            current_length = j - i + 1
-        max_length = max(max_length, current_length)
+                char_set.add(s[j])
+                current_count += 1
+        max_length = max(max_length, current_count)
     return max_length
 
 
-def length_of_longest_substring(s):
-    l = 0                 
-    max_length = 0        
-    char_set = set()      
-    for r in range(len(s)):
-        while s[r] in char_set:
-            char_set.remove(s[l])
-            l += 1
-
-        char_set.add(s[r])
-        
-        current_length = r - l + 1
-        
+def lengthLongestSubstring(s):
+    max_length = 0
+    left = 0
+    char_set = set()
+    for right in range(len(s)):
+        current_char = s[right]
+        while current_char in char_set:
+            char_set.remove(current_char)
+            left += 1
+        char_set.add(current_char)
+        current_length = right - left + 1
         max_length = max(max_length, current_length)
     return max_length
 
-s = "zxyzxyz"
-print(lengthLongestSubstring(s))
-print(length_of_longest_substring(s))
+    
+
