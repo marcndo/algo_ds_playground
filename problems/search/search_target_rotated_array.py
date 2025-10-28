2eb43d89005e64e00c40a294ca47f0f13bb0904e
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-10-28 01:52:29 +0100

    Refactor search in a rotated array

--- problems/search/search_target_rotated_array.py
+++ problems/search/search_target_rotated_array.py
@@ -1,59 +1,22 @@
-"""
-https://leetcode.com/problems/search-in-rotated-sorted-array/description/
-There is an integer array nums sorted in ascending order (with distinct values).
 
-Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
-
-Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
-
-You must write an algorithm with O(log n) runtime complexity.
-
-
-
-Example 1:
-
-Input: nums = [4,5,6,7,0,1,2], target = 0
-Output: 4
-Example 2:
-
-Input: nums = [4,5,6,7,0,1,2], target = 3
-Output: -1
-Example 3:
-
-Input: nums = [1], target = 0
-Output: -1
-
-
-Constraints:
-
-1 <= nums.length <= 5000
--104 <= nums[i] <= 104
-All values of nums are unique.
-nums is an ascending array that is possibly rotated.
--104 <= target <= 104
-"""
-
-def find_target_from_rotated_array(arr, target):
-    left, right = 0, len(arr) - 1
-
+def find_in_rotated_array(nums, target):
+    length = len(nums) - 1
+    left , right = 0, length
     while left <= right:
         mid = (left + right) // 2
-        if target == arr[mid]:
-            return mid
-        # left sorted portion of the array
-        if arr[left] < arr[mid]:
-            if target > arr[mid] or target < arr[left]:
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target and target <= nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] <= target and target <= right:
                 left = mid + 1
             else:
                 right = mid - 1
-        # right sorted portion of the array
-        if target < arr[mid] or target > arr[right]:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return - 1
+    return mid
 
 
-print(find_target_from_rotated_array([4,5,6,7,0,1,2],0))
+print(find_in_rotated_array([4,5,6,7,0,1,2],0))
 
 
