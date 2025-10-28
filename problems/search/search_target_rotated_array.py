cf3c678857248e44e43e8032a0522835c685c304
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-10-28 18:07:48 +0100

    Add solution for rotated array1

--- problems/search/search_target_rotated_array.py
+++ problems/search/search_target_rotated_array.py
@@ -1,4 +1,3 @@
-
 def find_in_rotated_array(nums, target):
     length = len(nums) - 1
     left , right = 0, length
@@ -18,7 +17,27 @@ def find_in_rotated_array(nums, target):
                 right = mid - 1
     return -1
 
+def search_in_rotated_array(nums, target):
+    left, right = 0, len(nums) - 1
 
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return True
+        if nums[left] == nums[mid] == nums[right]:
+            left += 1
+            right -= 1
+        elif nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+    return False
+print(search_in_rotated_array([4,5,6,7,0,1,2], 90))
 print(find_in_rotated_array([4,5,6,7,0,1,2], 1))
-
-
