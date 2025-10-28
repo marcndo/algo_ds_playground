3d0ac364326c68e7b1faa6b2ffd408004cb4be46
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-10-28 02:13:36 +0100

    Fix bug in search in rotated array problem

--- problems/search/search_target_rotated_array.py
+++ problems/search/search_target_rotated_array.py
@@ -4,19 +4,21 @@ def find_in_rotated_array(nums, target):
     left , right = 0, length
     while left <= right:
         mid = (left + right) // 2
-        if nums[left] <= nums[mid]:
-            if nums[left] <= target and target <= nums[mid]:
+        if nums[mid] == target:
+            return mid
+        elif nums[left] <= nums[mid]:
+            if nums[left] <= target and target < nums[mid]:
                 right = mid - 1
             else:
                 left = mid + 1
         else:
-            if nums[mid] <= target and target <= right:
+            if nums[mid] < target and target <= nums[right]:
                 left = mid + 1
             else:
                 right = mid - 1
-    return mid
+    return -1
 
 
-print(find_in_rotated_array([4,5,6,7,0,1,2],0))
+print(find_in_rotated_array([4,5,6,7,0,1,2], 1))
 
 
