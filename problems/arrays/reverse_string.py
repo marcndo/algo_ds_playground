b20c360b2f85e119236fad7b054ce26a913642cb
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-11-01 09:25:51 +0100

    Reverse a string

--- problems/arrays/reverse_string.py
+++ problems/arrays/reverse_string.py
@@ -1,16 +1,14 @@
-"""https://leetcode.com/problems/reverse-string/: create a functions that reverse a string
-Hi my name is Somebody should be ydobemoS si eman ym iH"""
+def reverse_string(s):
+    l , r = 0, len(s) - 1
+    while l < r:
+        current = s[l]
+        s[l] = s[r]
+        s[r] = current
+        l += 1
+        r -= 1
+    return s
 
 
-def reverse_string(st):
-    if not isinstance(st, list) or not all(isinstance(i, str) and len(i) == 1 for i in st):
-        return "double check the input and ensure it's an array of character"
-    left, right = 0, len(st) - 1
-    while left < right:
-        st[left], st[right] = st[right], st[left]
-        left += 1
-        right -= 1
-    return st
-
-
-print(reverse_string(['A', 'p', 'p', 'l', 'e']))
+s = ["h","e","l","l","o"]
+s = reverse_string(s)
+print(s)
