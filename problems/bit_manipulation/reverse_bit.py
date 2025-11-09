8949af4d9da5485329a4532074b96af6c55c3200
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-11-09 02:10:54 +0100

    Refactor reverse bits

--- problems/bit_manipulation/reverse_bit.py
+++ problems/bit_manipulation/reverse_bit.py
@@ -2,8 +2,8 @@ def reverse_bit(n):
     result = 0
     for i in range(32):
         bit = (n >> i) & 1
-        result = result | (bit << (31 - i))
+        result |=(bit << (31 - i))
     return result
 
-print(reverse_bit(11111111111111111111111111111101))
+
 
