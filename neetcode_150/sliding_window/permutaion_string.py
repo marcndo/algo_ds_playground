69e8ac8c089428ece82798d8c73955cab52f77ba
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-11-11 03:49:04 +0100

    Optimized solution for permuatation problem

--- neetcode_150/sliding_window/permutaion_string.py
+++ neetcode_150/sliding_window/permutaion_string.py
@@ -1,12 +1,3 @@
-from collections import Counter
-
-"""
-        input two strings lowercase
-        output: boolean
-        s1 = "abc", s2 = "lecabee"
-        output = true
-"""
-
 def check_inclusion(s1, s2):
     s1_length = len(s1)
     s2_length = len(s2)
@@ -41,18 +32,40 @@ def check_inclusion_optimal(s1, s2):
     return False
 
 
+def permutation_string(s1, s2):
+    countS1, countS2 = [0] * 26, [0] * 26
+    for i in range(len(s1)):
+        countS1[ord(s1[i]) - ord('a')]
+        countS2[ord(s2[i]) - ord('a')]
 
+    matches = 0
+    for i in range(26):
+        matches += (1 if countS1[i] == countS2[i] else 0)
+    l = 0
+    for r in range(len(s1), len(s2)):
+      if matches == 26:
+         return True
 
-    
-  
-                    
+    index = ord(s2[r]) - ord('a')
+    countS2[index] += 1
+    if countS1[index] == countS2[index]:
+         matches += 1
+    elif countS1[index] + 1 == countS2[index]:
+                matches -= 1
 
-            
-                
-    
-  
+    index = ord(s2[l]) - ord('a')
+    countS2[index] -= 1
+    if countS1[index] == countS2[index]:
+                matches += 1
+    elif countS1[index] - 1 == countS2[index]:
+         matches -= 1
+         l += 1
+    return matches == 26
 
 s1 = "abc"
 s2 = "lecabee"
-print(check_inclusion(s1,s2))
+
+print(check_inclusion(s1, s2))
 print(check_inclusion_optimal(s1, s2))
+print(permutation_string(s1, s2))
+
